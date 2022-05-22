fe0bf8fcd02a3ac5196cbdf9c99adf63ccdc924e
89YAMAL <alex-popov1987@yandex.ru>
2022-05-22 14:57:19 +0500

    Arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,9 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
+  let result = arr1.length === arr2.length && arr1.every((item, idx) => item === arr2[idx]);
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
+  let resultArr = arr.filter((positiveNumbers) => positiveNumbers > 0).filter((number) => number % 3 === 0).map((muliplicationTen) => muliplicationTen * 10);
   return resultArr; // array
 }
