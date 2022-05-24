d3c0766129b95972a020d60e2056f07c22f554c7
89YAMAL <alex-popov1987@yandex.ru>
2022-05-24 22:06:43 +0500

    Object

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,47 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function (...marks) {
+  this.marks = [...marks];
+}
+
+Student.prototype.getAverage = function () {
+  let result = this.marks.reduce((acc, item, idx, arr) => {
+    if (idx === arr.length - 1) {
+      return (acc + item) / arr.length;
+    } else {
+      return acc + item;
+    }
+  })
+  return result;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
+
+let student1 = new Student("Alex", "male", "35");
+student1.setSubject("Algebra");
+student1.addMarks(5, 4, 3, 5, 2);
+student1.exclude('low grades')
+console.log(student1)
+
+
+let student2 = new Student("Oleg", "male", "25");
